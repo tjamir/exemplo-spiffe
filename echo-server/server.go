38b1c3f4a496fab970ef788d1967cce084e9ff33
Thiago Jamir <tjamir@gmail.com>
2021-12-18 14:23:30 -0300

    server funcionando

--- echo-server/server.go
+++ echo-server/server.go
@@ -3,54 +3,89 @@ package main
 import (
 	"bufio"
 	"context"
+	"encoding/json"
 	"fmt"
 	"net"
+	"os"
+	"time"
 
 	"github.com/spiffe/go-spiffe/v2/spiffeid"
+	"github.com/spiffe/go-spiffe/v2/spiffetls"
+	"github.com/spiffe/go-spiffe/v2/spiffetls/tlsconfig"
+	"github.com/spiffe/go-spiffe/v2/workloadapi"
 )
 
 type ServerConfig struct {
-	Port         int
-	AuthorizedId string
+	Port         int    `json:"port"`
+	AuthorizedId string `json:"authorized_id"`
+	SocketPath   string `json:"socket_path"`
 }
 
 func main() {
 
-	ctx, cancel := context.WithTimeout(context.Background(), 10)
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
 
 	defer cancel()
+	serverConfig, err := loadConfig()
 
-	serverConfig := ServerConfig{
-		Port:         8080,
-		AuthorizedId: "",
+	if err != nil {
+		panic(err)
 	}
 
-	RunServer(ctx, serverConfig)
+	err = RunServer(ctx, serverConfig)
+	if err != nil {
+		panic(err)
+	}
 
 }
 
+func loadConfig() (ServerConfig, error) {
+	serverConfig := ServerConfig{}
+	configFile, err := os.Open("conf/echo-server.json")
+	if err != nil {
+		return serverConfig, err
+	}
+	defer configFile.Close()
+	jsonParser := json.NewDecoder(configFile)
+	err = jsonParser.Decode(&serverConfig)
+	if err != nil {
+		return serverConfig, err
+	}
+
+	return serverConfig, nil
+}
+
 func RunServer(ctx context.Context, config ServerConfig) error {
+	allowedId, err := spiffeid.FromString(config.AuthorizedId)
+	if err != nil {
+		return err
+	}
 
-	allowedId := spiffeid.MustSpiffeID("spiffe://example.org/spire/agent/server")
+	fmt.Println("Iniciando tls server")
+	listen, err := spiffetls.ListenWithMode(ctx,
+		"tcp", fmt.Sprintf(":%d", config.Port),
+		spiffetls.MTLSServerWithSourceOptions(tlsconfig.AuthorizeID(allowedId),
+			workloadapi.WithClientOptions(workloadapi.WithAddr(config.SocketPath))))
 
-	listen, err := net.Listen("tcp", fmt.Sprintf(":%d", config.Port))
 	if err != nil {
 		return err
 	}
 
 	for {
+		fmt.Println("Entrou no loop")
 		connection, err := listen.Accept()
+		fmt.Println("Aceitou")
 		go func() {
 			defer connection.Close()
 			if err != nil {
 				return
 			}
+			fmt.Println("Rodando a conexão")
 			if err := handleConnection(connection); err != nil {
 				return
 			}
 		}()
 	}
-	return nil
 
 }
 
